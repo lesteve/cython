4cdd57570f60f8ef87dc71acc6449551584898d8
Loïc Estève <loic.esteve@ymail.com>
2018-09-02 18:40:47 +0200

    Use simple function with typed memoryview

--- tests/memoryview/const_fused_type_memoryview.pyx
+++ tests/memoryview/const_fused_type_memoryview.pyx
@@ -2,19 +2,7 @@
 # mode: run
 # tag: memoryview
 
-import numpy as np
-cimport numpy as np
-
-cimport cython
 from cython cimport floating
 
-ARRAY = np.arange(10).astype('float')
-ARRAY.flags.writeable = False
-
-
-def test_function(const floating[:] arr):
-    """
-    >>> test_function(ARRAY)
-    4
-    """
-    return 4
+def test_function(const floating number):
+    pass
